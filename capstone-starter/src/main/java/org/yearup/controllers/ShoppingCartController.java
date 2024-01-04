6014b0d33ef15538031d5592b3eef05875612d54
Shankar Ale Magar <shankaralemagar91@gmail.com>
2024-01-04 10:06:42 -0500

    Fixed bugs

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -63,6 +63,7 @@ public class ShoppingCartController
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
     @PostMapping("/products/{productId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public void addItemToCart(Principal principal, @PathVariable int productId, HttpServletResponse response)
     {
         if(principal == null || principal.getName() == null)
@@ -93,9 +94,29 @@ public class ShoppingCartController
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+     @PutMapping("/products/{productId}")
+     public void updateQuantity(Principal principal)
+     {
+         try {
+             // get the currently logged in username
+             String userName = principal.getName();
+             // find database user by userId
+             User user = userDao.getByUserName(userName);
+             int userId = user.getId();
+         } catch (Exception e) {
+             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+         }
+     }
 
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
+    @DeleteMapping("")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+            public void deleteAll(Principal principal)
+    {
+        
+
+    }
 
 }
